18045ac47f97659e3624ebc7afea30e83d79ac96
Saurabh Prajapati <165933863+sp-108@users.noreply.github.com>
2026-02-11 12:25:56 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
